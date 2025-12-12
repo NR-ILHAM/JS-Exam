c53dcdb45614d6a904865986d26d99250e462b04
unknown <nrilhamttp13@gmail.com>
2025-12-12 20:37:31 +0300

    Updated Files

--- problem1.js
+++ problem1.js
@@ -1,28 +1,25 @@
 
-const CIN=91; 
+
+  
+const CIN = 11;
 let isPrime = true;
 
 function validateIdentity(CIN) 
 {
 
-  if (CIN < 2)
- {
-    isPrime = false;
- } else 
-  {
-    for (let i = 2; i * i <= CIN; i++) 
-    {
-      if (CIN % i === 0) 
-        {
-        isPrime = false;
-        break;
-        }
+if (CIN < 2) {
+  isPrime = false;
+} else {
+  for (let i = 2; i < CIN; i++) {
+    if (CIN % i === 0) {
+      isPrime = false;
     }
   }
 }
 
+}
+
 validateIdentity(CIN);
- 
+
 console.log(isPrime ? "Agent Approved: Identity pattern is untraceable." : "Rejected: Identity pattern shows external links");
-  
 
