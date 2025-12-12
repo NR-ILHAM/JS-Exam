f33d3cbda20ec7394e3cf8d970011423cd8dcdb2
unknown <nrilhamttp13@gmail.com>
2025-12-12 19:29:51 +0300

    Initial commit

---
+++ problem2.js
@@ -0,0 +1,12 @@
+
+const temporalCode="2024867728";
+const len=temporalCode.length;
+console.log(len);
+if(len>=10)
+{
+    console.log("Security Level: Omega" );
+}else{
+
+    console.log("Security Level: Sigma" );
+}
+
